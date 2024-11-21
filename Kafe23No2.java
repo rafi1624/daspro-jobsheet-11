6838e784f72255dc980447e51713798aad960ec5
Rafi Abyantara Pratama <rafiabyantarapratama@gmail.com>
2024-11-21 11:33:02 +0700

    Modifikasi Percobaan 2 Pertanyaan 5

--- Kafe23No2.java
+++ Kafe23No2.java
@@ -1,11 +1,19 @@
 public class Kafe23No2 {
-    public static void Menu(String namaPelanggan,boolean isMember) {
+    public static void Menu(String namaPelanggan,boolean isMember, String kodePromo) {
         System.out.println("Selamat Datang, " + namaPelanggan + "!");
 
         if (isMember) {
             System.out.println("Anda Adalah Member, Dapatkan Diskon 10% Untuk Setiap Pembelian!");
             
         }
+        if (kodePromo.equalsIgnoreCase("diskon 50")) {
+            System.out.println("Selamat! Anda Mendapatkan Diskon 50%");   
+        }else if (kodePromo.equalsIgnoreCase("diskon 30")) {
+            System.out.println("Selamat! Anda Mendapatkan Diskon 30%");    
+        }else{
+            System.out.println("Kode Tidak Valid");
+        }
+           
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappucino - Rp 20,000");
@@ -18,7 +26,11 @@ public class Kafe23No2 {
        
     }
     public static void main(String[] args) {
-        Menu("Andi", true);
+        Menu("Andi",true, "diskon 50");
+        System.out.println();
+        Menu("Rafi", false, "diskon 30");
+        System.out.println();
+        Menu("Wildan", false, "diskon 100");
     }
     
 }
